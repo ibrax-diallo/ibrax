322222d1a645a09d3dc8a7ec3716149fe2040bf8
ancien <iibdiallo@gmail.com>
2019-11-10 20:52:54 +0100

    project test

---
+++ build/precache-manifest.135fc9982123a8f5c355af16de7260fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f545b3050ae40a24a6f9d27775f0d1bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c2c8e188eeb1e67fce0",
+    "url": "/static/css/2.a1fd0458.chunk.css"
+  },
+  {
+    "revision": "4f90c47fbc62f01f3d6d",
+    "url": "/static/css/main.ab53c6c1.chunk.css"
+  },
+  {
+    "revision": "0c2c8e188eeb1e67fce0",
+    "url": "/static/js/2.869fac9d.chunk.js"
+  },
+  {
+    "revision": "4f90c47fbc62f01f3d6d",
+    "url": "/static/js/main.af40306c.chunk.js"
+  },
+  {
+    "revision": "ac41fd8eae069239c6c4",
+    "url": "/static/js/runtime-main.e3973f46.js"
+  }
+]);
